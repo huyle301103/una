5bcd980c2202fa3f35ad2e63a1bc191220e1be24
huyle301103 <lpduchuy301103@gmail.com>
2026-03-28 00:18:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
-    "Please??? 💔",
-    "Don't do this to me...",
-    "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Thiệc hok 🤔",
+    "Đi mừ 🥺",
+    "Đi mừuuuuuu...",
+    "Tớ bùn áaaaaa... 😢",
+    "Năn nỉiiiiii 💔",
+    "Oeeeeeeee...",
+    "Cậu hok thưnnn tớoooo 😭",
+    "Ai choa mè bấm heheeee 😜"
 ]
 
 const yesTeasePokes = [
-    "try saying no first... I bet you want to know what happens 😏",
-    "go on, hit no... just once 👀",
-    "you're missing out 😈",
-    "click no, I dare you 😏"
+    "Thử bấm No i 😏",
+    "Thử bấm Hok i 👀",
+    "Thử cái i 😈",
+    "Thử no i 😏"
 ]
 
 let yesTeasedCount = 0
